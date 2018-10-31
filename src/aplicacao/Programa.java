d7e62e438d867ee46b272cfa61bf6250aa8b3924
danilovalerio <danilo_vs@hotmail.com>
2018-10-31 17:13:06 -0300

    Adicionado o enunciado e a classe programa somente

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,34 @@
+package aplicacao;
+
+import java.util.Scanner;
+
+/*
+ * Na contagem de votos de uma eleição, são gerados vários registros de votação 
+ * contendo o nome do candidato e a quantidade de votos (formato .csv) que 
+ * ele obteve em uma urna de votação. 
+ * Você deve fazer um programa para ler os registros de votação 
+ * a partir de um arquivo, e daí gerar um relatório consolidado 
+ * com os totais de cada candidato.
+ */
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		//Scanner sc = new Scanner(System.in);
+		String path = "C:\\Users\\dsilva\\Documents\\eleicoes-simulador.csv";
+		
+		try {
+			
+		} catch (Exception e) {
+			System.out.println("Erro: "+ e.getMessage());
+		}
+		
+		//sc.close();
+		
+		
+		
+
+	}
+
+}
